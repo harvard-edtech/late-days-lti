cdd3a6dfb0f228b330a4fc43b4504a4fe2d7b395
Anita Lam <anita.lam@tufts.edu>
2019-11-09 13:05:52 -0500

    Finished adding all ItemList tests

--- client/src/Body/shared/ItemList/ItemFooter.spec.js
+++ client/src/Body/shared/ItemList/ItemFooter.spec.js
@@ -5,7 +5,7 @@ import assert from 'assert';
 import ItemFooter from './ItemFooter';
 
 describe('client > src > Body > shared > ItemList > ItemFooter', () => {
-  it('Checks ItemRows object exists and sorts by Due At date', async () => {
+  it('Checks ItemFooter object exists', async () => {
     // Initializes driver with ItemFooter object
     const driver = new Driver(
       <ItemFooter
@@ -18,7 +18,7 @@ describe('client > src > Body > shared > ItemList > ItemFooter', () => {
 
     // Checks that message is correct
     assert.equal(
-      driver.getText('.itemname-container'),
+      driver.getText('.itemfooter-container'),
       'This is a footer',
       'footer text was incorrect'
     );
